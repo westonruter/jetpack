0562867aa2d26a58370ed7ee837c3da52ac57c96
Brandon Kraft <public@brandonkraft.com>
2018-11-02 16:18:38 -0500

    Add initial debugger library structure. No functional change.

---
+++ _inc/lib/debugger/0-load.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Loading the various functions used for Jetpack Debugging.
+ *
+ * @package Jetpack.
+ */
+
+/* The "In-Plugin Debugger" admin page. */
+require_once( 'class.jetpack-debugger.php' );
