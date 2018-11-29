d3aca85cd262fe0c021294b432b2aa8d315bd3ef
Jefferson Rabb <jeff@atavist.net>
2018-11-28 23:30:56 -0500

    Register Slideshow block and its dependencies.

--- modules/blocks.php
+++ modules/blocks.php
@@ -31,3 +31,28 @@ function jetpack_map_block_load_assets( $attr, $content ) {
 	Jetpack_Gutenberg::load_assets_as_required( 'map', $dependencies );
 	return $content;
 }
+
+jetpack_register_block(
+	'slideshow',
+	array(
+		'render_callback' => 'jetpack_slideshow_block_load_assets',
+	)
+);
+
+/**
+ * Map block registration/dependency declaration.
+ *
+ * @param array  $attr - Array containing the map block attributes.
+ * @param string $content - String containing the map block content.
+ *
+ * @return string
+ */
+function jetpack_slideshow_block_load_assets( $attr, $content ) {
+	$dependencies = array(
+		'lodash',
+		'wp-element',
+		'wp-i18n',
+	);
+	Jetpack_Gutenberg::load_assets_as_required( 'slideshow', $dependencies );
+	return $content;
+}
